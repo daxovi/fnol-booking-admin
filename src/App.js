c6da360a5f28d6d3fd131e94d50c2f019dfeb33b
Dalibor Janeček <dalibor.janecek@gmail.com>
2023-12-26 10:12:33 +0100

    admin ui

--- src/App.js
+++ src/App.js
@@ -1,47 +1,201 @@
 import React, { useState, useEffect } from 'react'
-import { nacteniTicketu } from './components/api';
+import { nacteniTicketu, updateNotes, finishTicket } from './components/api';
+import './App.css';
 
 const App = () => {
-
   const [tickets, setTickets] = useState([]);
+  const [searchBoxText, setSearchBoxText] = useState("");
+  const [selectedTicket, setSelectedTicket] = useState();
+  const [notes, setNotes] = useState("");
+  const [newNotes, setNewNotes] = useState("");
+
+  const [isSaving, setIsSaving] = useState(false);
+  const [saveSuccess, setSaveSuccess] = useState(null);
+  const [errorMessage, setErrorMessage] = useState('');
+
   const nowDate = Date.now();
 
   useEffect(() => {
+    nacteniDB();
+  }, [])
+
+  const nacteniDB = () => {
     nacteniTicketu()
-    .then(data => {
+      .then(data => {
         setTickets(data)
       })
       .catch(error => {
         console.error('Error fetching data:', error);
       });
-}, [])
+  }
 
-const dateToText = (miliseconds) => { 
-  const date = new Date(miliseconds);
-  const formatedDate = date.toLocaleString()
-  return formatedDate;
- }
+  const dateToText = (miliseconds) => {
+    const date = new Date(miliseconds);
+    const formatedDate = date.toLocaleString()
+    return formatedDate;
+  }
+
+  const saveNotes = async (id, notes, event) => {
+    event.stopPropagation();
+    setIsSaving(true);
+    setSaveSuccess(null);
+    setErrorMessage('');
+
+    try {
+      await updateNotes(id, notes);
+      setSaveSuccess(true);
+      setTimeout(() => {
+        setSaveSuccess(null);
+      }, 5000);
+    } catch (error) {
+      setSaveSuccess(false);
+      setErrorMessage(error.message);
+    } finally {
+      nacteniDB();
+      setIsSaving(false);
+    }
+  }
+
+  const validateTicket = async (id, event) => {
+    event.stopPropagation();
+    try {
+      await finishTicket(id, 1800000000000);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSelectedTicket();
+      nacteniDB();
+    }
+  }
+
+  const deleteTicket = async (id, event) => { 
+    event.stopPropagation();
+    try {
+      await finishTicket(id, 18000000);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSelectedTicket();
+      nacteniDB();
+    }
+  }
+
+  const editToggle = (index, ticket) => {
+    if (selectedTicket == ticket._id) {
+      setSelectedTicket();
+    } else {
+      setSelectedTicket(ticket._id);
+      setNewNotes(ticket.notes)
+      setNotes(ticket.notes);
+    }
+  }
 
   return (
-    <div>Aktuální vstupenky: 
-      <div>
-      {
-  tickets
-    .filter(ticket => ticket.date > nowDate)
-    .map((ticket, index) => (
-      <div key={index}>Číslo vstupenky: {ticket.ticket}, email: {ticket.email}, datum: {dateToText(ticket.date)}</div>
-    ))
-}
-    </div>
-    Expirované vstupenky:
-    {
-  tickets
-    .filter(ticket => ticket.date < nowDate)
-    .map((ticket, index) => (
-      <div key={index}>Číslo vstupenky: {ticket.ticket}, email: {ticket.email}, datum: {dateToText(ticket.date)}</div>
-    ))
-}
+    <div>
+      <div className="">
+        <input onChange={(event) => { setSearchBoxText(event.target.value) }} type="text" name="" id="" /> {selectedTicket}
       </div>
+      <div>
+        Aktuální vstupenky:
+        <div className="ticket-row ticket-header">
+          <div className="ticket ticket-nr">číslo vstupenky</div>
+          <div className="ticket ticket-email">e-mail</div>
+          <div className="ticket ticket-date">datum expirace</div>
+        </div>
+        {
+          tickets
+            .filter(ticket => ticket.date > nowDate && ticket.date < 1790000000000 && ticket.email.includes(searchBoxText))
+            .map((ticket, index) => (
+              <div className="ticket-row" key={index} onClick={() => { editToggle(index, ticket) }}>
+              <div className="ticket ticket-nr">{ticket.ticket}</div>
+                <div className="ticket ticket-email">{ticket.email}</div>
+                <div className="ticket ticket-date">{dateToText(ticket.date)}
+                <button className='btn btn-positive' onClick={(event) => { validateTicket(ticket._id, event) }}>vyzvednout</button>
+                </div>
+                {selectedTicket == ticket._id &&
+                  <div className="ticket-edit-row">
+                    <textarea onClick={(event) => { event.stopPropagation() }} defaultValue={notes} onChange={(event) => { setNewNotes(event.target.value) }} name="" id="" cols="30" rows="3"></textarea>
+                  </div>
+                }
+                {selectedTicket == ticket._id &&
+                  <div className="ticket-edit-row ticket-controls">
+                    {!isSaving && !saveSuccess && <button onClick={() => { saveNotes(ticket._id, newNotes) }}>uložit</button>}
+                    {isSaving && <p>ukládání...</p>}
+                    {saveSuccess && <p>uloženo</p>}
+                    {saveSuccess === false && <p>chyba při ukládání: {errorMessage}</p>}
+                  </div>
+                }
+              </div>
+            ))
+        }
+      </div>
+      <div>
+        Vyzvednuté vstupenky:
+        <div className="ticket-row ticket-header">
+          <div className="ticket ticket-nr">číslo vstupenky</div>
+          <div className="ticket ticket-email">e-mail</div>
+          <div className="ticket ticket-date">datum expirace</div>
+        </div>
+        {
+          tickets
+            .filter(ticket => ticket.date > 1790000000000 && ticket.email.includes(searchBoxText))
+            .map((ticket, index) => (
+              <div className="ticket-row" key={index} onClick={() => { editToggle(index, ticket) }}>
+              <div className="ticket ticket-nr">{ticket.ticket}</div>
+                <div className="ticket ticket-email">{ticket.email}</div>
+                <div className="ticket ticket-date">{dateToText(ticket.date)}
+                  <button className='btn btn-negative' onClick={(event) => { deleteTicket(ticket._id, event) }}>odstranit</button>
+                </div>
+                {selectedTicket == ticket._id &&
+                  <div className="ticket-edit-row">
+                    <textarea onClick={(event) => { event.stopPropagation() }} defaultValue={notes} onChange={(event) => { setNewNotes(event.target.value) }} name="" id="" cols="30" rows="3"></textarea>
+                  </div>
+                }
+                {selectedTicket == ticket._id &&
+                  <div className="ticket-edit-row ticket-controls">
+                    {!isSaving && !saveSuccess && <button className='btn' onClick={(event) => { saveNotes(ticket._id, newNotes, event) }}>uložit</button>}
+                    {isSaving && <p>ukládání...</p>}
+                    {saveSuccess && <p>uloženo</p>}
+                    {saveSuccess === false && <p>chyba při ukládání: {errorMessage}</p>}
+                  </div>
+                }
+              </div>
+            ))
+        }
+      </div>
+      Expirované vstupenky:
+      <div className="ticket-row ticket-header">
+        <div className="ticket ticket-nr">číslo vstupenky</div>
+        <div className="ticket ticket-email">e-mail</div>
+        <div className="ticket ticket-date">datum expirace</div>
+      </div>
+      {
+        tickets
+          .filter(ticket => ticket.date < nowDate && ticket.email.includes(searchBoxText))
+          .map((ticket, index) => (
+            <div className="ticket-row" key={index} onClick={() => { editToggle(index, ticket) }}>
+              <div className="ticket ticket-nr">{ticket.ticket}</div>
+              <div className="ticket ticket-email">{ticket.email}</div>
+              <div className="ticket ticket-date">{dateToText(ticket.date)}
+                <button className='btn btn-positive' onClick={(event) => { validateTicket(ticket._id, event) }}>vyzvednout</button>
+              </div>
+              {selectedTicket == ticket._id &&
+                <div className="ticket-edit-row">
+                  <textarea onClick={(event) => { event.stopPropagation() }} defaultValue={notes} onChange={(event) => { setNewNotes(event.target.value) }} name="" id="" cols="30" rows="3"></textarea>
+                </div>
+              }
+              {selectedTicket == ticket._id &&
+                <div className="ticket-edit-row ticket-controls">
+                  {!isSaving && !saveSuccess && <button onClick={(event) => { saveNotes(ticket._id, newNotes, event) }}>uložit</button>}
+                  {isSaving && <p>ukládání...</p>}
+                  {saveSuccess && <p>uloženo</p>}
+                  {saveSuccess === false && <p>chyba při ukládání: {errorMessage}</p>}
+                </div>
+              }
+            </div>
+          ))
+      }
+    </div>
   )
 }
 
--- src/components/api.js
+++ src/components/api.js
@@ -71,3 +71,47 @@ export const nacteniTicketu = async () => {
         throw error;
     }
 }
+
+export const updateNotes = async (id, notes) => { 
+    const url = process.env.REACT_APP_BACKEND + `/save-ticket/${id}`;
+
+    try {
+        const response = await fetch(url, {
+            method: 'PATCH',
+            body: JSON.stringify({
+                ["notes"]: notes
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        })
+
+        const data = await response.json();
+        console.log(data);
+
+    } catch (err) {
+        console.log("error: ", err)
+    }
+ }
+
+ export const finishTicket = async (id, time) => { 
+    const url = process.env.REACT_APP_BACKEND + `/save-ticket/${id}`;
+
+    try {
+        const response = await fetch(url, {
+            method: 'PATCH',
+            body: JSON.stringify({
+                ["date"]: time
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        })
+
+        const data = await response.json();
+        console.log(data);
+
+    } catch (err) {
+        console.log("error: ", err)
+    }
+ }
